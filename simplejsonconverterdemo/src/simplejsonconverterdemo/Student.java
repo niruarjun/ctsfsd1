3e16de83ebb6c6d4195a03874130aa1c918437ad
niruarjun <59592224+niruarjun@users.noreply.github.com>
2020-02-04 17:07:17 +0530

    jsontoobject Converter

---
+++ simplejsonconverterdemo/src/simplejsonconverterdemo/Student.java
@@ -0,0 +1,24 @@
+package simplejsonconverterdemo;
+
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+@EqualsAndHashCode
+@Getter
+@Setter
+	public class Student {
+		private int id;
+		private String firstName;
+		private String lastName;
+		
+
+	}
+
+
