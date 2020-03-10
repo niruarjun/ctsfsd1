4f93b6b8db4a897eca3a6adb13aeb877e70566c8
niruarjun <59592224+niruarjun@users.noreply.github.com>
2020-03-10 17:20:30 +0530

    security pswd encription

---
+++ boot-spring-security-pswdencryption/src/main/java/com/example/demo/BootSpringSecurityPswdencryptionApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootSpringSecurityPswdencryptionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BootSpringSecurityPswdencryptionApplication.class, args);
+	}
+
+}
